320954824d6d6ab444ecdec7970bd121ad198ca0
seongtak <zergyst1988@naver.com>
2016-10-23 20:44:58 +0900

    [WAT] 암호화와 트랜잭션
    - Bcrypt 암호화 (단방향)사용
    - 트랜잭션 구현(Exception 하위 예외모두 트랜잭션 롤백처리됨)

--- src/main/java/com/taky/and/wendy/user/service/UserServiceImpl.java
+++ src/main/java/com/taky/and/wendy/user/service/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.taky.and.wendy.user.service;
 
+import java.util.Base64;
+import java.util.Date;
+
+import org.mindrot.jbcrypt.BCrypt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.util.StringUtils;
 
 import com.taky.and.wendy.user.exception.UserSignupException;
@@ -37,10 +42,19 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
+	@Transactional(rollbackFor = Exception.class)
 	public void saveUser(User user) throws Exception {
 		this.validUserParameter(user.getEmail(), "### 회원가입시 email은 반드시 존재해야합니다.");
 		this.validUserParameter(user.getPassword(), "### 회원가입시 비밀번호는 반드시 존재해야합니다.");
 		this.validUserParameter(user.getName(), "### 회원가입시 이름은 반드시 존재해야합니다.");
+		
+		/*
+		 * BCrypt 암호화 참고
+		 * - http://d2.naver.com/helloworld/318732
+		 * - http://jsonobject.tistory.com/239
+		 */
+		user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
+		user.setCertification(this.createCertification(user.getName()));
 		
 		userMapper.insertUser(user);
 	}
@@ -50,5 +64,18 @@ public class UserServiceImpl implements UserService {
 			logger.error(errorMessage);
 			throw new UserSignupException(errorMessage);
 		}
+	}
+	
+	/**
+	 * 유저의 이메일에 인증 URL을 보내기 위한 인증데이터
+	 * 
+	 * @param name
+	 * @return
+	 */
+	private String createCertification(String name) {
+		String normal = new Date().getTime() + name;
+		String encrypt = BCrypt.hashpw(normal, BCrypt.gensalt());
+		
+		return Base64.getEncoder().encodeToString(encrypt.getBytes());
 	}
 }
