d9006d0a59de30d22bd41ace49875bbc4acf1872
seongtak <zergyst1988@naver.com>
2016-11-05 21:51:03 +0900

    [WAT] 섬네일 파일 올리기

--- src/main/java/com/taky/and/wendy/config/SpringConfig.java
+++ src/main/java/com/taky/and/wendy/config/SpringConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring4.SpringTemplateEngine;
@@ -23,5 +24,18 @@ public class SpringConfig extends WebMvcConfigurerAdapter {
 		templateEngine.setTemplateResolver(templateResolver);
 		templateEngine.addDialect(new SpringSecurityDialect());
 		return templateEngine;
+	}
+	
+	/**
+	 * file upload resolver
+	 * - http://stackoverflow.com/questions/25830503/spring-mvc-http-status-400-required-multipartfile-parameter-file-is-not-pre
+	 * - http://java.ihoney.pe.kr/351
+	 * - CommonsMultipartResolver쓰면 안된다.
+	 * @return
+	 */
+	@Bean
+	public StandardServletMultipartResolver multipartResolver() {
+		StandardServletMultipartResolver resolver = new StandardServletMultipartResolver();
+		return resolver;
 	}
 }
--- src/main/java/com/taky/and/wendy/upload/controller/UploadController.java
+++ src/main/java/com/taky/and/wendy/upload/controller/UploadController.java
@@ -1,15 +1,59 @@
 package com.taky.and.wendy.upload.controller;
 
+import java.io.File;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class UploadController {
+	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+	
+	@Value("${wat.file.home}")
+	private String fileHome;
+	
 	@PreAuthorize("hasRole('1')")
 	@RequestMapping(path = "/upload/date-course", method = RequestMethod.GET)
 	public String showUploadDateCourse() {
 		return "/upload/date";
+	}
+	
+	@RequestMapping(path = "/upload/date-course", method = RequestMethod.POST)
+	@ResponseBody
+	public String uploadThumbnail(@RequestParam("thumbnail") MultipartFile thumbnail) {
+		logger.debug("파일업로드시작");
+		
+		if (thumbnail.isEmpty()) {
+			return "파일 업로드에 실패하였습니다.";
+		} 
+		
+		try {
+			logger.debug(thumbnail.getOriginalFilename());
+			
+			File file = new File(fileHome + "/static/image/upload");
+			
+			if (file.isDirectory() == false) {
+				file.mkdirs();
+			}
+			logger.debug(file.getAbsolutePath());
+			logger.debug(file.toURI().toString());
+			
+			File upload = new File(file.getAbsolutePath() + "/" + thumbnail.getOriginalFilename());
+			
+			logger.debug(upload.getAbsolutePath());
+			thumbnail.transferTo(upload);
+			
+		} catch(Exception e) {
+			logger.error("### 파일을 업로드하는 과정에서 문제가 발생하였습니다!", e);
+		}
+		return "업로드 성공";
 	}
 }
